821ffce8d19e897029c63d1ddd24303fa4feb403
Philippe scorsolini <pscorso93@gmail.com>
2018-04-16 14:49:05 +0200

    reading from file not stdin

--- code/wordcount_mapReduce.hs
+++ code/wordcount_mapReduce.hs
@@ -4,18 +4,19 @@ import Pipes
 import Control.Monad (forever)
 import Data.HashMap.Strict
 import Data.Text (pack, unpack, toLower)
+import System.IO
 --
 -- wordcount mapReduce-style, only benefit is to reduce memory consumption
 --
 
 main :: IO ()
-main = runEffect (P.fold (\x a -> insertWith (+) a 1 x) 
+main =  withFile "test-text.txt" ReadMode $ \file -> runEffect (P.fold (\x a -> insertWith (+) a 1 x) 
                         empty 
                         (show . toList) 
-                        (P.stdinLn >-> 
+                        (P.fromHandle file >-> 
                             P.map (splitOn " ") >-> 
                             forever (await >>= each) >->
                             P.map (unpack . toLower . pack)
                             )
-                        ) 
+                        )
         >>= putStrLn
